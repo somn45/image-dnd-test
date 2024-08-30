138ad725c348c59a133f8ba9fd32cf3bf5644e4e
somn45 <somnsomn45@gmail.com>
2024-08-30 16:49:12 +0900

    이미지 드래그 인 드롭 삽입 & 미리보기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const ImageBox = styled.div`
+  width: 360px;
+  height: 360px;
+  border: 2px solid black;
+`;
+
+export default function App() {
+  const [previewImg, setPreviewImg] = useState('');
+
+  console.log(previewImg);
+
+  const handleImageDnd = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const formData = new FormData();
+    const file = e.target.files;
+    if (!file) return;
+    console.log(file);
+  };
+
+  const handleImageDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  /*
+  const handleImageDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!e.dataTransfer) return;
+    const file = e.dataTransfer.files[0];
+    console.log(e.dataTransfer.files);
+    const imageUrl = URL.createObjectURL(file);
+    setPreviewImg(imageUrl);
+  };
+  */
+
+  const handleImageDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (!e.dataTransfer) return;
+
+    const file = e.dataTransfer.files[0];
+    const formData = new FormData();
+    formData.append('image', file, 'avatar');
+    console.log(formData);
+  };
+
+  const resetImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setPreviewImg('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {previewImg ? (
+        <>
+          <img
+            src={previewImg}
+            style={{ width: '360px', height: '360px', position: 'relative' }}
+          />
+          <button
+            onClick={resetImage}
+            style={{
+              width: '120px',
+              height: '80px',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+            }}
+          ></button>
+        </>
+      ) : (
+        <div
+          onDragEnter={handleImageDragEnter}
+          onDrop={handleImageDrop}
+          style={{
+            width: '360px',
+            height: '360px',
+            border: '2px dashed black',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <form>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageDnd}
+              style={{ width: '360px', height: '360px' }}
+            />
+            <input type="submit" />
+          </form>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
